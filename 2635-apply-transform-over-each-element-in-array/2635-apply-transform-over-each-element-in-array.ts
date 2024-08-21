88ea6ae469fda8021ea4571463951939937fadc3
Abdulrhaman Mohamed <89229223+Abdulrhaman-Mohamed@users.noreply.github.com>
2024-08-21 15:37:44 +0300

    Time: 59 ms (42.22%), Space: 51.6 MB (6.25%) - LeetHub

--- 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.ts
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.ts
@@ -1,3 +1,8 @@
 function map(arr: number[], fn: (n: number, i: number) => number): number[] {
-    return arr.map((n,i)=>fn(n,i))
+    // return arr.map((n,i)=>fn(n,i))
+    const newArr = [];
+    for (let i = 0; i < arr.length; i++) 
+            newArr.push(fn(arr[i],i))
+    
+    return newArr;
 };
